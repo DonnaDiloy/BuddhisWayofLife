44dc2343f9a3c56a0f7eb2c726392d086295477f
DonnaDiloy <74063432+DonnaDiloy@users.noreply.github.com>
2021-02-05 20:16:18 +0800

    update

--- assets/js/BuddhistWayofLife.js
+++ assets/js/BuddhistWayofLife.js
@@ -23,3 +23,52 @@ quoteBtn.addEventListener('click',() =>{
     quote.innerHTML = quotes[random].quote;
     author.innerHTML = quotes[random].quote;
 })
+
+// var usingClass = document.getElementsByClassName("solution-using-class")[0];
+// usingClass.addEventListener('click',() => {
+//     const quotes = [
+//         {
+//             quote:
+//                "Work out your own salvation. Do not depend on others.",
+//             author: "-Buddha"
+//         },
+//         {
+//             quote:
+//                 "We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow",
+//             author: "-Buddha"
+//         },
+//     ]
+
+//     let random = Math.floor(Math.random() * quotes.length);
+
+//     quote.innerHTML = quotes[random].quote;
+//     author.innerHTML = quotes[random].quote;
+// })
+
+
+
+
+
+
+
+
+
+// // function anotherSolution() {
+// //     const quotes = [
+// //         {
+// //             quote:
+// //                "Work out your own salvation. Do not depend on others.",
+// //             author: "-Buddha"
+// //         },
+// //         {
+// //             quote:
+// //                 "We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow",
+// //             author: "-Buddha"
+// //         },
+// //     ]
+
+// //     let random = Math.floor(Math.random() * quotes.length);
+
+// //     quote.innerHTML = quotes[random].quote;
+// //     author.innerHTML = quotes[random].quote;
+// }
